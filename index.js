873c6504f5c57afbad4740fc2d231b6ba37f5950
Akash Meshram <akash.meshram@front.page>
2024-10-30 07:33:46 +0530

    feat(web): Add html to serve from server

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import http from "http";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;  
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// Serve the static HTML file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// For server health check
+app.get("/healthz", (_req, res) => {
+  res.send("OK");
 });
 
 // Start the server
